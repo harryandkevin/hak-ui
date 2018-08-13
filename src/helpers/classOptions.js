c2070c4cc197a43a57d52209bec2e6bf7bd2355c
Kevin Kuchar <kkuchar@vectorform.com>
2018-08-13 09:35:20 -0400

    feat(classOptions): updates classOptions to accept and array and adds error checking

--- src/helpers/classOptions.js
+++ src/helpers/classOptions.js
@@ -1,7 +1,25 @@
+/**
+ * Accepts an array of classNames or an object whose keys are
+ * classNames and values are booleans and returns a single
+ * string of classNames
+ * @param {*} options - Array or Object
+ * @return {String} - Space separated classNames
+ */
 export const classOptions = (options) => {
-    const classes = Object.keys(options);
-    
-    return classes.reduce((acc, val) => {
-        return `${acc} ${options[val] ? val : ''}`;
-    }, '').trim();
+	if (Array.isArray(options)) {
+		return options.join(',');
+	}
+
+	if (typeof options === 'object') {
+		const classes = Object.keys(options);
+		
+		return classes.reduce((acc, val) => {
+				return `${acc} ${options[val] ? val : ''}`;
+		}, '').trim();
+	}
+
+	const errorString = `classOptions error: Provided options must be an array or object. A ${typeof options} was provided.`;
+	console.warn(errorString);
+
+	return '';
 }
