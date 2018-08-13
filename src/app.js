50c21b303d6a7b1f81dd8ffcae5464b5d0d97f00
Kevin Kuchar <kkuchar@vectorform.com>
2018-08-13 09:37:21 -0400

    feat(IntroBanner): adds IntroBanner view

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import HeroBanner from './components/HeroBanner';
+import IntroBanner from './views/IntroBanner';
 import './styles/main.scss';
-
-// images
-import bannerImage from './assets/hak-banner.png';
-import logo from './assets/logo.png';
 
 class App extends Component {
   render() {
     return (
-      <HeroBanner background={bannerImage}>
-        <div className="center">
-          <img src={logo} />
-          <h1>2 devs / 1 dream</h1>
-        </div>
-      </HeroBanner>
+      <IntroBanner />
     );
   }
 }
