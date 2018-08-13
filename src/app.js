78c6ce7d5fb160a955156d48e1185c0bc326863c
Kevin Kuchar <kkuchar@vectorform.com>
2018-08-12 22:15:20 -0400

    feat(HeroBanner): adds background image to HeroBanner component

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import HeroBanner from './components/HeroBanner';
 import './styles/main.scss';
 
+// images
+import bannerImage from './assets/hak-banner.png';
+import logo from './assets/logo.png';
+
 class App extends Component {
   render() {
     return (
-      <HeroBanner>
-        <div>Yoyoyo</div>
+      <HeroBanner background={bannerImage}>
+        <div className="center">
+          <img src={logo} />
+          <h1>2 devs / 1 dream</h1>
+        </div>
       </HeroBanner>
     );
   }
